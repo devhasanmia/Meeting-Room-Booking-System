328c3f93a148b1a4add3dd09188a9c1f81710067
devhasanmia <dev.hasanmia@gmail.com>
2024-06-12 09:06:04 +0600

    Fix: Resolved issue #2 - created Room Interface and Room Model

---
+++ src/app/modules/room/room.interface.ts
@@ -0,0 +1,9 @@
+export type Troom = {
+    name: string;
+    roomNo: string;
+    floorNo: string;
+    capacity: number;
+    pricePerSlot: number;
+    amenities: string[];
+    isDeleted: boolean;
+}
