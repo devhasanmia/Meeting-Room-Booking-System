409f73995fcaf54b3e989035a580567b3eeb6d09
devhasanmia <dev.hasanmia@gmail.com>
2024-06-12 11:09:19 +0600

    Added Router and and User Controller and Service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { Request, Response } from "express";
+import { UserService } from "./user.service";
+
+
+const createUser = async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = await UserService.createUser(payload);
+    res.status(201).json({
+        success: true,
+        statusCode: 201,
+        message: "User Created Successfully!",
+        data: user
+    })
+}
+
+export const UserController = {
+    createUser
+}
